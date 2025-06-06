e2351e396142f624fd3f64e7df9d9abf7554782f
Rong "Mantle" Bao <webmaster@csmantle.top>
2025-06-06 22:55:10 +0800

    Add textures

--- tetris3d/tetris3d.py
+++ tetris3d/tetris3d.py
@@ -3,7 +3,9 @@ import math
 import random
 import sys
 import typing as ty
+import itertools as it
 
+from PIL import Image
 import OpenGL.GL as gl
 import OpenGL.GLU as glu
 import OpenGL.GLUT as glut
@@ -20,15 +22,12 @@ camera_pitch = 30.0
 mouse_last_pos = (0, 0)
 mouse_lb_down = False
 
-game_center = (
-    game.dims[0] / 2.0,  # Xm -> Xg
-    game.dims[2] / 2.0,  # Zm -> Yg
-    game.dims[1] / 2.0,  # Ym -> Zg
-)
+game_center = (game.dims[0] / 2.0, game.dims[2] / 2.0, game.dims[1] / 2.0)
 
+texture_block: ty.Optional[ty.Any] = None
 
 def draw_axes():
-    gl.glLineWidth(2.0)
+    gl.glLineWidth(1.0)
 
     gl.glColor3f(*COLOR_RED)
     gl.glBegin(gl.GL_LINE_STRIP)
@@ -60,31 +59,127 @@ def draw_axes():
     gl.glVertex3f(0.0, game.dims[2], 0.0)
     gl.glEnd()
 
+    for x, z, y in it.product(
+        range(GAME_AREA_SIZE[0]), range(GAME_AREA_SIZE[1]), range(GAME_AREA_SIZE[2])
+    ):
+        gl.glPushMatrix()
+        gl.glTranslatef(x, y, z)
+
+        gl.glBegin(gl.GL_LINES)
+
+        gl.glColor3f(*COLOR_RED)
+        gl.glVertex3f(0.0, 0.0, 0.0)
+        gl.glVertex3f(RENDER_BLOCK_SIZE / 3, 0.0, 0.0)
+
+        gl.glColor3f(*COLOR_GREEN)
+        gl.glVertex3f(0.0, 0.0, 0.0)
+        gl.glVertex3f(0.0, RENDER_BLOCK_SIZE / 3, 0.0)
+
+        gl.glColor3f(*COLOR_BLUE)
+        gl.glVertex3f(0.0, 0.0, 0.0)
+        gl.glVertex3f(0.0, 0.0, RENDER_BLOCK_SIZE / 3)
+
+        gl.glEnd()
+
+        gl.glPopMatrix()
+
     gl.glLineWidth(1.0)
 
 
 def draw_blocks():
+    gl.glEnable(gl.GL_TEXTURE_2D)
+    gl.glBindTexture(gl.GL_TEXTURE_2D, texture_block)
+
     for block in game.all_blocks:
         gl.glPushMatrix()
-        gl.glTranslatef(
-            float(block[0]) + 0.5,
-            float(block[2]) + 0.5,
-            float(block[1]) + 0.5,
-        )
-        gl.glColor(*COLOR_RED)
-        glut.glutSolidCube(1.0)
+        gl.glTranslatef(float(block[0]), float(block[2]), float(block[1]))
+        gl.glScalef(RENDER_BLOCK_SIZE, RENDER_BLOCK_SIZE, RENDER_BLOCK_SIZE)
+
+        gl.glColor(*COLOR_WHITE)
+        gl.glBegin(gl.GL_QUADS)
+
+        # Top face (y = 1.0)
+        gl.glNormal3f(0.0, 1.0, 0.0)
+        gl.glTexCoord2f(0.0, 0.0)
+        gl.glVertex3f(0.0, 1.0, 0.0)
+        gl.glTexCoord2f(1.0, 0.0)
+        gl.glVertex3f(1.0, 1.0, 0.0)
+        gl.glTexCoord2f(1.0, 1.0)
+        gl.glVertex3f(1.0, 1.0, 1.0)
+        gl.glTexCoord2f(0.0, 1.0)
+        gl.glVertex3f(0.0, 1.0, 1.0)
+
+        # Bottom face (y = 0.0)
+        gl.glNormal3f(0.0, -1.0, 0.0)
+        gl.glTexCoord2f(0.0, 0.0)
+        gl.glVertex3f(0.0, 0.0, 1.0)
+        gl.glTexCoord2f(1.0, 0.0)
+        gl.glVertex3f(1.0, 0.0, 1.0)
+        gl.glTexCoord2f(1.0, 1.0)
+        gl.glVertex3f(1.0, 0.0, 0.0)
+        gl.glTexCoord2f(0.0, 1.0)
+        gl.glVertex3f(0.0, 0.0, 0.0)
+
+        # Left face (x = 0.0)
+        gl.glNormal3f(-1.0, 0.0, 0.0)
+        gl.glTexCoord2f(0.0, 0.0)
+        gl.glVertex3f(0.0, 0.0, 0.0)
+        gl.glTexCoord2f(1.0, 0.0)
+        gl.glVertex3f(0.0, 0.0, 1.0)
+        gl.glTexCoord2f(1.0, 1.0)
+        gl.glVertex3f(0.0, 1.0, 1.0)
+        gl.glTexCoord2f(0.0, 1.0)
+        gl.glVertex3f(0.0, 1.0, 0.0)
+
+        # Right face (x = 1.0)
+        gl.glNormal3f(1.0, 0.0, 0.0)
+        gl.glTexCoord2f(0.0, 0.0)
+        gl.glVertex3f(1.0, 0.0, 1.0)
+        gl.glTexCoord2f(1.0, 0.0)
+        gl.glVertex3f(1.0, 0.0, 0.0)
+        gl.glTexCoord2f(1.0, 1.0)
+        gl.glVertex3f(1.0, 1.0, 0.0)
+        gl.glTexCoord2f(0.0, 1.0)
+        gl.glVertex3f(1.0, 1.0, 1.0)
+
+        # Front face (z = 1.0)
+        gl.glNormal3f(0.0, 0.0, 1.0)
+        gl.glTexCoord2f(0.0, 0.0)
+        gl.glVertex3f(0.0, 0.0, 1.0)
+        gl.glTexCoord2f(1.0, 0.0)
+        gl.glVertex3f(1.0, 0.0, 1.0)
+        gl.glTexCoord2f(1.0, 1.0)
+        gl.glVertex3f(1.0, 1.0, 1.0)
+        gl.glTexCoord2f(0.0, 1.0)
+        gl.glVertex3f(0.0, 1.0, 1.0)
+
+        # Back face (z = 0.0)
+        gl.glNormal3f(0.0, 0.0, -1.0)
+        gl.glTexCoord2f(0.0, 0.0)
+        gl.glVertex3f(1.0, 0.0, 0.0)
+        gl.glTexCoord2f(1.0, 0.0)
+        gl.glVertex3f(0.0, 0.0, 0.0)
+        gl.glTexCoord2f(1.0, 1.0)
+        gl.glVertex3f(0.0, 1.0, 0.0)
+        gl.glTexCoord2f(0.0, 1.0)
+        gl.glVertex3f(1.0, 1.0, 0.0)
+
+        gl.glEnd()
+
         gl.glPopMatrix()
+
+    gl.glDisable(gl.GL_TEXTURE_2D)
 
 
 def repose_camera():
     gl.glLoadIdentity()
-    eye_x = game_center[0] + camera_distance * math.cos(
-        math.radians(camera_pitch)
-    ) * math.sin(math.radians(camera_yaw))
+    eye_x = game_center[0] + camera_distance * math.cos(math.radians(camera_pitch)) * math.sin(
+        math.radians(camera_yaw)
+    )
     eye_y = game_center[1] + camera_distance * math.sin(math.radians(camera_pitch))
-    eye_z = game_center[2] + camera_distance * math.cos(
-        math.radians(camera_pitch)
-    ) * math.cos(math.radians(camera_yaw))
+    eye_z = game_center[2] + camera_distance * math.cos(math.radians(camera_pitch)) * math.cos(
+        math.radians(camera_yaw)
+    )
     glu.gluLookAt(
         eye_x,
         eye_y,
@@ -175,6 +270,8 @@ def handle_idle():
 
 
 def main():
+    global texture_block
+
     logging.basicConfig(level=logging.DEBUG)
     random.seed(0x0D000721)
 
@@ -191,6 +288,28 @@ def main():
     gl.glEnable(gl.GL_BLEND)
     gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
     gl.glLineWidth(1.0)
+
+    with Image.open(PATH_TEXTURE_BLOCK) as img:
+        img_width, img_height = img.size
+        texture_block_data = img.tobytes("raw", "RGB", 0, -1)
+    texture_block = gl.glGenTextures(1)
+    gl.glBindTexture(gl.GL_TEXTURE_2D, texture_block)
+    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_REPEAT)
+    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_REPEAT)
+    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR_MIPMAP_LINEAR)
+    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
+    gl.glTexImage2D(
+        gl.GL_TEXTURE_2D,
+        0,
+        gl.GL_RGB,
+        img_width,
+        img_height,
+        0,
+        gl.GL_RGB,
+        gl.GL_UNSIGNED_BYTE,
+        texture_block_data,
+    )
+    gl.glGenerateMipmap(gl.GL_TEXTURE_2D)
 
     glut.glutDisplayFunc(handle_display)
     glut.glutReshapeFunc(handle_reshape)
